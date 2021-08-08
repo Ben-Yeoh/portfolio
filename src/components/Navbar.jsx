b60e08c0868e3cbf5c4349503e6c3c098e4431be
z5257289 <z5257289@gitlab.cse.unsw.edu.au/>
2021-08-08 17:06:34 +1000

    Add styling to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,64 @@ const Option = styled(Link)`
   text-decoration: none;
 `;
 
-const Navigation = styled.div`
-  width: 35%;
+const Navigation = styled.ul`
+  margin: 0;
+  padding: 0;
   display: flex;
-  justify-content: space-between;
+  justify-self: end;
+  font-size: 1.2em;
+  a,
+  button {
+    padding: 1rem 3rem;
+    display: flex;
+    align-items: center;
+    position: relative;
+    font-weight: 400;
+    font-size: 1em;
+    background: none;
+    border: 0;
+    cursor: pointer;
+    @media (max-width: 700px) {
+      font-size: 10px;
+      padding: 0 10px;
+    }
+    &:before {
+      content: '';
+      width: 2px;
+      background: grey;
+      height: 100%;
+      left: 0;
+      position: absolute;
+      transform: skew(-20deg);
+      top: 0;
+      bottom: 0;
+    }
+    &:after {
+      height: 2px;
+      background: ${props => props.theme.secondary};
+      content: '';
+      width: 0;
+      position: absolute;
+      transform: translateX(-50%);
+      transition: width 0.4s;
+      transition-timing-function: cubic-bezier(1, -0.65, 0, 2.31);
+      left: 50%;
+      margin-top: 2rem;
+    }
+    &:hover,
+    &:focus {
+      outline: none;
+      &:after {
+        width: calc(100% - 60px);
+      }
+    }
+  }
+  @media (max-width: 1300px) {
+    border-top: 1px solid grey;
+    width: 100%;
+    justify-content: center;
+    font-size: 1.5rem;
+  }
 `;
 
 const Main = styled.main`
@@ -21,6 +75,7 @@ const Main = styled.main`
   top: 0;
   padding-top: 50px;
   padding-right: 100px;
+  backdrop-filter: blur(3px);
 `;
 
 const Navbar = () => {
